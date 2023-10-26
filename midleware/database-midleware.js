3142d914ba62d0024306a45f8e3ef4d3cf7694a5
aghfairawan <58446116+aghfairawan@users.noreply.github.com>
2023-10-26 12:04:35 +0700

    Update database-midleware.js

--- midleware/database-midleware.js
+++ midleware/database-midleware.js
@@ -9,15 +9,6 @@ const databaseMidleware = async (req, res, next)=>{
 };
 
 
-// const databaseMidleware = async () => {
-//     try {
-//       const client = await new MongoClient(process.env.MONGO_URI).connect();
-//       const db = client.db(process.env.MONGO_DB);
-//       console.log( `<=================== CONNECT TO DB ==================`);
-//       return db
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+
 
 module.exports = databaseMidleware
